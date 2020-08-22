73191897f43472ab8caec1883b58f751b272c884
sawzedong <saw.zedong@dhs.sg>
2020-08-22 10:51:17 +0800

    Initial Commit

---
+++ clicker-improved/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clicker-improved
+//
+//  Created by Shaw Sheng Saw on 22/8/20.
+//  Copyright © 2020 NIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
